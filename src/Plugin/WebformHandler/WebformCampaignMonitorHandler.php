14a8314dcff78582b2b96d56aa7cfeb5f83805ef
Sonny Kieu <sonny@salsadigital.com.au>
2017-01-20 15:52:14 +1100

    Handle uncaught exceptions.

--- src/Plugin/WebformHandler/WebformCampaignMonitorHandler.php
+++ src/Plugin/WebformHandler/WebformCampaignMonitorHandler.php
@@ -86,98 +86,126 @@ class WebformCampaignMonitorHandler extends WebformHandlerBase {
    * {@inheritdoc}
    */
   public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
-    $lists = $this->campaignMonitor->getLists();
+    try {
+      $lists = $this->campaignMonitor->getLists();
 
-    $options = array();
-    $options[''] = $this->t('- Select a list -');
-    foreach ($lists as $list_id => $list) {
-      $options[$list_id] = $list['name'];
-    }
-
-    $form['list'] = [
-      '#type' => 'select',
-      '#title' => $this->t('List'),
-      '#required' => TRUE,
-      '#default_value' => $form_state->getValue('list') ?: $this->configuration['list'],
-      '#options' => $options,
-      '#ajax' => [
-        // 'callback' => ['::listChangeCallback'],
-        // 'wrapper' => 'webform-campaignmonitor-mapping',
-      ],
-    ];
-
-    $fields = $this->getWebform()->getElementsFlattenedAndHasValue();
-
-    $options = array();
-    $options[''] = $this->t('- Select an email field -');
-    foreach ($fields as $field_name => $field) {
-      if ($field['#type'] == 'email') {
-        $options[$field_name] = $field['#title'];
-      }
-    }
-
-    $form['email'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Email field'),
-      '#required' => TRUE,
-      '#default_value' => $form_state->getValue('email') ?: $this->configuration['email'],
-      '#options' => $options,
-    ];
-
-    $options = array();
-    $options[''] = $this->t('- Select a text field -');
-    foreach ($fields as $field_name => $field) {
-      if ($field['#type'] == 'textfield' || $field['#type'] == 'value') {
-        $options[$field_name] = $field['#title'] ?: '[' . $field_name . ']';
-      }
-    }
-
-    $form['fullname'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Full name field'),
-      '#required' => FALSE,
-      '#default_value' => $form_state->getValue('fullname') ?: $this->configuration['fullname'],
-      '#options' => $options,
-    ];
-
-    $form['mapping'] = [
-      '#type' => 'details',
-      '#title' => $this->t('Custom fields'),
-      '#open' => TRUE,
-      '#attributes' => ['id' => 'webform-campaignmonitor-mapping'],
-    ];
-
-    $list_id = $form_state->getValue('list') ?: ($form_state->getValue(['settings', 'list']) ?: $this->configuration['list']);
-    if (!empty($list_id)) {
       $options = array();
-      $options[''] = $this->t('- Select a field -');
-      foreach ($fields as $field_name => $field) {
-        $options[$field_name] = $field['#title'] ?: '[' . $field_name . ']';
+      $options[''] = $this->t('- Select a list -');
+      foreach ($lists as $list_id => $list) {
+        $options[$list_id] = $list['name'];
       }
 
-      $customFields = $this->campaignMonitor->getCustomFields($list_id);
-      foreach ($customFields as $key => $customField) {
-        $key = str_replace(array('[', ']'), '', strtolower($key));
-        $autoMapping = isset($options[$key]) ? $key : '';
-        $defaultMapping = $form_state->getValue(['mapping', $key]);
-        if (!$defaultMapping) {
-          $defaultMapping = $this->configuration['mapping'][$key];
-          if (!$defaultMapping) {
-            $defaultMapping = $autoMapping;
-          }
+      $form['list'] = [
+        '#type' => 'select',
+        '#title' => $this->t('List'),
+        '#required' => TRUE,
+        '#default_value' => $form_state->getValue('list') ?: $this->configuration['list'],
+        '#options' => $options,
+        '#ajax' => [
+          /*
+           * {@code}
+           * 'callback' => [$this, 'listChangeCallback'],
+           * 'wrapper' => 'webform-campaignmonitor-mapping',
+           * {@endcode}
+           */
+        ],
+      ];
+
+      $fields = $this->getWebform()->getElementsFlattenedAndHasValue();
+
+      $options = array();
+      $options[''] = $this->t('- Select an email field -');
+      foreach ($fields as $field_name => $field) {
+        if ($field['#type'] == 'email') {
+          $options[$field_name] = $field['#title'];
         }
-        $form['mapping'][$key] = [
-          '#type' => 'select',
-          '#title' => $customField['FieldName'] . ' (' . $customField['Key'] . ')',
-          '#options' => $options,
-          '#default_value' => $defaultMapping,
-        ];
       }
+
+      $form['email'] = [
+        '#type' => 'select',
+        '#title' => $this->t('Email field'),
+        '#required' => TRUE,
+        '#default_value' => $form_state->getValue('email') ?: $this->configuration['email'],
+        '#options' => $options,
+      ];
+
+      $options = array();
+      $options[''] = $this->t('- Select a text field -');
+      foreach ($fields as $field_name => $field) {
+        if ($field['#type'] == 'textfield' || $field['#type'] == 'value') {
+          $options[$field_name] = $field['#title'] ?: '[' . $field_name . ']';
+        }
+      }
+
+      $form['fullname'] = [
+        '#type' => 'select',
+        '#title' => $this->t('Full name field'),
+        '#required' => FALSE,
+        '#default_value' => $form_state->getValue('fullname') ?: $this->configuration['fullname'],
+        '#options' => $options,
+      ];
+
+      $form['mapping'] = [
+        '#type' => 'details',
+        '#title' => $this->t('Custom fields'),
+        '#open' => TRUE,
+        '#prefix' => '<div id="webform-campaignmonitor-mapping">',
+        '#suffix' => '</div>',
+      ];
+
+      $list_id = $form_state->hasValue('list')
+        ? $form_state->getValue('list')
+        : ($form_state->hasValue(['settings', 'list'])
+          ? $form_state->getValue(['settings', 'list'])
+          : $this->configuration['list']
+        );
+      if (!empty($list_id)) {
+        $options = array();
+        $options[''] = $this->t('- Select a field -');
+        foreach ($fields as $field_name => $field) {
+          $options[$field_name] = $field['#title'] ?: '[' . $field_name . ']';
+        }
+
+        $customFields = $this->campaignMonitor->getCustomFields($list_id);
+        foreach ($customFields as $key => $customField) {
+          $key = str_replace(array('[', ']'), '', strtolower($key));
+          $autoMapping = isset($options[$key]) ? $key : '';
+          $defaultMapping = $form_state->getValue(['mapping', $key]);
+          if (!$defaultMapping) {
+            $defaultMapping = $this->configuration['mapping'][$key];
+            if (!$defaultMapping) {
+              $defaultMapping = $autoMapping;
+            }
+          }
+          $form['mapping'][$key] = [
+            '#type' => 'select',
+            '#title' => $customField['FieldName'] . ' (' . $customField['Key'] . ')',
+            '#options' => $options,
+            '#default_value' => $defaultMapping,
+          ];
+        }
+      }
+
+    }
+    catch (\Exception $exception) {
+      watchdog_exception('campaignmonitor', $exception);
+      drupal_set_message($exception->getMessage(), 'error');
     }
 
     return $form;
   }
 
+  /**
+   * Ajax call back.
+   *
+   * @param array $form
+   *   Form array.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   Form state.
+   *
+   * @return array
+   *   Form element array.
+   */
   public function listChangeCallback(array &$form, FormStateInterface $form_state) {
     return isset($form['mapping']) ? $form['mapping'] : $form['settings']['mapping'];
   }
@@ -215,13 +243,18 @@ class WebformCampaignMonitorHandler extends WebformHandlerBase {
       }
       $email = $fields['data'][$email_field];
       $fullname = $fullname_field ? $fields['data'][$fullname_field] : '';
-      if (!$this->campaignMonitor->subscribe($this->configuration['list'], $email, $fullname, $sendFields)) {
-        $errors = $this->campaignMonitor->getErrors();
-        foreach ($errors as $error) {
-          /** @var \Drupal\Core\StringTranslation\TranslatableMarkup $message */
-          $message = $error['message'];
-          drupal_set_message($message->render(), 'error');
+      try {
+        if (!$this->campaignMonitor->subscribe($this->configuration['list'], $email, $fullname, $sendFields)) {
+          $errors = $this->campaignMonitor->getErrors();
+          foreach ($errors as $error) {
+            /** @var \Drupal\Core\StringTranslation\TranslatableMarkup $message */
+            $message = $error['message'];
+            drupal_set_message($message->render(), 'error');
+          }
         }
+      }
+      catch (\Exception $exception) {
+        watchdog_exception('campaignmonitor', $exception);
       }
     }
   }
